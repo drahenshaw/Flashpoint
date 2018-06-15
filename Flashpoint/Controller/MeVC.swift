853400ed1aa4e5b591acca0bb0f3fe09e4d4f1bb
drahenshaw <drahenshaw@gmail.com>
2018-06-15 14:58:11 -0700

    Created profile VC

---
+++ Flashpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  Flashpoint
+//
+//  Created by David Henshaw on 6/15/18.
+//  Copyright © 2018 David Henshaw. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func signOutPressed(_ sender: Any) {
+    }
+    
+
+}
