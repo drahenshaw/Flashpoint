735dc660b591b4f3925294a41b6a4b740ad982b7
drahenshaw <drahenshaw@gmail.com>
2018-06-15 11:32:22 -0700

    Initial Commit

---
+++ Flashpoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Flashpoint
+//
+//  Created by David Henshaw on 6/15/18.
+//  Copyright © 2018 David Henshaw. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
