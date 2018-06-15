609a1a3b719f1c6787ac005aa42ce45142af1306
drahenshaw <drahenshaw@gmail.com>
2018-06-15 13:27:59 -0700

    Firebase register and login methods

---
+++ Flashpoint/Services/AuthService.swift
@@ -0,0 +1,39 @@
+//
+//  AuthService.swift
+//  Flashpoint
+//
+//  Created by David Henshaw on 6/15/18.
+//  Copyright © 2018 David Henshaw. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registeruser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let  user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.user.providerID, "email": user.user.email]
+            DataService.instance.createDBUser(uid: user.user.uid, userData: userData)
+            userCreationComplete(true, nil)
+            
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, error) in
+            guard let user = authDataResult else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
