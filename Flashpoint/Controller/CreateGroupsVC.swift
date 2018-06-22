e39d573f2ab77fc7525ea04361b72ddb3db3c6f1
drahenshaw <drahenshaw@gmail.com>
2018-06-22 12:32:45 -0700

    Updated group UI & methods

--- Flashpoint/Controller/CreateGroupsVC.swift
+++ Flashpoint/Controller/CreateGroupsVC.swift
@@ -18,20 +18,64 @@ class CreateGroupsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addMemberLabel: UILabel!
     
-    
-    
+    var emailArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        emailSearchTextField.delegate = self
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextField.text!) { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            }
+        }
     }
 
     @IBAction func doneButtonPressed(_ sender: Any) {
+        
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     
+    
+    
 }
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
+        return cell
+    }
+    
+}
+
+extension CreateGroupsVC: UITextFieldDelegate {
+    
+    
+    
+}
+
